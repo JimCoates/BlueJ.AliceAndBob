40a8ed4dcdad308657f1b5efe2cfa915f239c25a
James Coates <jamescoates@zipcoders-MacBook-Pro.local>
2019-02-05 17:03:32 -0500

    commit changes to main file

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args ){
-
+        Scanner scanner1 = new Scanner( System.in);
+        System.out.print("Enter your first name:");
+        String name = scanner1.nextLine();
+        
+        if (name.equals("Alice")) {
+            System.out.println("Hello Alice");
+        } 
+        else if (name.equals("Bob")) {
+            System.out.println("Hello Bob");
+        } 
+        else {
+            System.out.println("Sorry you are not Alice or Bob, goodbye");
+        }
+        
     }
 }
+
